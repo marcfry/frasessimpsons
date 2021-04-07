3092701e26811ccc3b7269808f3097c0ebadfae1
Marcelo Frias <frias.marcelo@gmail.com>
2021-04-07 18:09:01 -0300

    se agrego la logica para hacer el request a la api y mostrar los datos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
+import {useState, useEffect} from "react";
+
 function App() {
+//creo el State
+const [personaje, setPersonaje] = useState({})
+
+useEffect(()=>{
+  //logica a ejecutar
+  consultarApi();
+
+},[]);
+
+const consultarApi = async()=>{
+const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+const resultado = await respuesta.json();
+console.log(respuesta);
+console.log(resultado[0]);
+//guardar objeto dentro del state
+setPersonaje(resultado[0]);
+}
+
   return (
     <section className="container text-center my-5">
       <article className="d-flex flex-column align-items-center">
@@ -11,11 +31,11 @@ function App() {
           alt="logo de los simpsons"
           className="w-75"
         />
-        <Button variant="warning" className="my-5">
+        <Button variant="warning" className="my-5" onClick={()=> consultarApi()}>
           Obtener frase
         </Button>
       </article>
-      <Frase/>
+      <Frase personaje={personaje}/>
 
     </section>
   );
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
-const Frase = () => {
+const Frase = (props) => {
     return (
         <Card>
     <Card.Body>
         <div className="row">
         <div className="col-sm-12 col-md-4">
-<Card.Title>Personaje</Card.Title>
+            <img src={props.personaje.image} alt={props.personaje.Cardcharacter}/>
+
         </div>
         <div className="col-sm-12 col-md-8">
+            <Card.Title>{props.personaje.character}</Card.Title>
 <Card.Text>
-      Frase Aleatoria
+    {props.personaje.quote}
     </Card.Text>
         </div>
         </div>
-    
-    
+     
      </Card.Body>
 </Card>
     );

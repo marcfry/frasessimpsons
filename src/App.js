69dfd9aa8d03bb84a5c4e94b0f6b3d0e2fd216c6
Marcelo Frias <frias.marcelo@gmail.com>
2021-04-13 17:31:11 -0300

    terminado proyecto de frases

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
-//creo el State
-const [personaje, setPersonaje] = useState({})
+  //creo el State
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
 
-useEffect(()=>{
-  //logica a ejecutar
-  consultarApi();
+  useEffect(() => {
+    //logica a ejecutar
+    consultarApi();
+  }, []);
 
-},[]);
+  const consultarApi = async () => {
+    setCargando(true);
+    const respuesta = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes"
+    );
+    const resultado = await respuesta.json();
+    console.log(respuesta);
+    console.log(resultado[0]);
 
-const consultarApi = async()=>{
-const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
-const resultado = await respuesta.json();
-console.log(respuesta);
-console.log(resultado[0]);
+    setTimeout(()=>{
 //guardar objeto dentro del state
-setPersonaje(resultado[0]);
-}
+    setPersonaje(resultado[0]);
+    setCargando(false);
+    },2500)
+   
+  };
+  //operador ternario (condicion) ? (lo que quiero hacer si la condición es verdadera) : (lo que quiero hacer si la condicion es falsa)
+  const mostrarComponente =
+    cargando === true ? <Spinner /> : <Frase personaje={personaje} />;
 
   return (
     <section className="container text-center my-5">
@@ -31,12 +43,15 @@ setPersonaje(resultado[0]);
           alt="logo de los simpsons"
           className="w-75"
         />
-        <Button variant="warning" className="my-5" onClick={()=> consultarApi()}>
+        <Button
+          variant="warning"
+          className="my-5"
+          onClick={() => consultarApi()}
+        >
           Obtener frase
         </Button>
       </article>
-      <Frase personaje={personaje}/>
-
+      {mostrarComponente}
     </section>
   );
 }
